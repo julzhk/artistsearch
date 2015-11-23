0ab7e8339db30c9f22f6d10a372753f9085ac9e2
Julian Harley <julian.harley@pancentric.com>
2015-11-23 17:24:53 +0000

    add test for api view

--- artist_search/search/tests.py
+++ artist_search/search/tests.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
-from search.views import home_page
+from search.views import home_page, api_page
 from django.http import HttpRequest
 
 class HomePageTest(TestCase):
@@ -15,3 +15,10 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>Artist Search API</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+class APITest(TestCase):
+
+    def test_root_url_resolves_to_api_view(self):
+        found = resolve('/api')
+        self.assertEqual(found.func, api_page)
+
