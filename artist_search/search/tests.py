cd0055c6ae1fd19c7775c78a28b486174359020a
Julian Harley <julian.harley@pancentric.com>
2015-11-23 17:16:38 +0000

    homepage url resolves

--- artist_search/search/tests.py
+++ artist_search/search/tests.py
@@ -5,5 +5,5 @@ from search.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/') 
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
